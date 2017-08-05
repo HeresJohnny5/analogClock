61afb608fa1e37a724b1828a068dad55aaac20e9
John Erickson <ericksonjohn08@hotmail.com>
2017-08-05 19:39:25 -0400

    Placed all code from prior push into function runTheClock using a setInterval to invoke the function each second. Commented out the transition property for id hour, minute, and second.

--- analogClock/script.js
+++ analogClock/script.js
@@ -2,19 +2,23 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-var date = new Date();
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log("Hour " + hr + ": " + min + " minutes" + ": " + sec + " seconds.");
+function runTheClock() {
+	var date = new Date();
+	let hr = date.getHours();
+	let min = date.getMinutes();
+	let sec = date.getSeconds();
+	console.log("Hour " + hr + ": " + min + " minutes" + ": " + sec + " seconds.");
 
-let hrPosition = (hr * 360/12) + (min * (360/60)/12);
-/* (current hour * clock degrees/12 hours on the clock) + (current minute * (clock degrees/60)/60) */
-let minPosition = (min * 360/60) + (sec * (360/60)/60);
-/* (current minute * clock degrees/60 minutes in a hour) + (current seconds * (clock degrees/60)/60) */
-let secPosition = sec * 360/60;
-// current seconds * clock degrees/60 seconds in a minute
+	let hrPosition = (hr * 360/12) + (min * (360/60)/12);
+	/* (current hour * clock degrees/12 hours on the clock) + (current minute * (clock degrees/60)/60) */
+	let minPosition = (min * 360/60) + (sec * (360/60)/60);
+	/* (current minute * clock degrees/60 minutes in a hour) + (current seconds * (clock degrees/60)/60) */
+	let secPosition = sec * 360/60;
+	// current seconds * clock degrees/60 seconds in a minute
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+	HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+	MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+	SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock, 1000);
